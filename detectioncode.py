d7c081a4da70fc3a859cd47c5df3a4ef2cc91d8e
Varun <58021274+varun141199@users.noreply.github.com>
2020-08-10 13:34:39 +0530

    Add files via upload

---
+++ detectioncode.py
@@ -0,0 +1,27 @@
+import cv2
+
+# Load the cascade
+
+fcas = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+
+# Read the input image
+
+pic = cv2.imread("test.jpg")
+
+# Convert into grayscale
+
+gray_img = cv2.cvtColor(pic, cv2.COLOR_BGR2GRAY)
+
+# Detect faces
+
+faces = fcas.detectMultiScale(gray_img, scaleFactor=1.05, minNeighbors=5)
+
+# Draw rectangle around the faces
+
+for (x, y, w, h) in faces:
+    cv2.rectangle(pic, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+# Display the output
+
+cv2.imshow('img', pic)
+cv2.waitKey()
